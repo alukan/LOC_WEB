7c219beed2e3121b137cdb945b62a63b22b908fa
alukan <kolyaterenya@gmail.com>
2024-02-08 11:26:25 +0100

    added readme with homework

--- lesson1_async_and_await/example_with_html/test_callback.js
+++
@@ -1,7 +0,0 @@
-
-alert("2") // will be called when the script is loaded
-
-
-function hello(){
-    return "5"
-}
---
+++ lesson1_async_and_await/example_with_html/test_scrtipt.js
@@ -0,0 +1,7 @@
+
+alert("3") // will be called when the script is loaded
+
+
+function hello(){
+    return "5"
+}
