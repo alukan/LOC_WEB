73ef33838e935e29e0c9effa96f36bcc3ac2f709
alukan <kolyaterenya@gmail.com>
2024-03-18 15:17:12 +0100

    lesson 7, continue with the project

--- to_do_list_project/README.md
+++ to_do_list_project/README.md
@@ -1,3 +1,18 @@
 ## Tamplate 
 
 https://excalidraw.com/#room=ec45020ab64ba14f9495,BUtEZwPlq0PiwIC9bRrmSA
+
+## Plan
+
+- create user part. It will call '/tasks' and save it in array "tasks"
+- add ID to the list
+- Put tasks in list (use Document.getElementById)
+  Finished
+
+## Plan, part 2
+
+- update list when we get answer from server
+
+## POST request
+
+https://excalidraw.com/#json=kwK7prkDC1XFQfa7Pqs_W,T4j3J8v4yAwNROvtvsxjcA
--- to_do_list_project/server.js
+++ to_do_list_project/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 
+
+app.use(express.json());
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-const tasks = ["task from server1", "task from server2", "task from server3"];
+let tasks = ["task from server1", "task from server2", "task from server3"];
 
 // if user uses /tasks, he will get all tasks
 app.get("/tasks", (request, response) => {
@@ -14,10 +17,23 @@ app.get("/tasks", (request, response) => {
   response.end(JSON.stringify({ tasks: tasks }));
 });
 
-app.get('/', (request, response) =>{
-response.sendFile(__dirname+'/index.html') // C:\Users\..\project
-//C:\Users\..\project\server.js
-})
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html"); // C:\Users\..\project
+  //C:\Users\..\project\server.js
+});
+
+app.post("/addTask", (request, response) => {
+  console.log(request.body);
+  const newTask = request.body.task;
+  if (newTask) {
+    tasks.push(newTask);
+    response.writeHead(200, { "Content-Type": "applicaation/json" });
+    response.end(JSON.stringify({ message: "everything is ok" }));
+  } else {
+    response.writeHead(400);
+    response.end();
+  }
+});
 
 // app.get('/html', ((request, response) => {
 //     response.sendFile(__dirname + '/index.html');
