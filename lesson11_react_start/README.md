2f7d8a8d2f9770aae6159989fd3cf62df2e333f8
alukan <kolyaterenya@gmail.com>
2024-04-25 22:46:41 +0200

    lesson 12, continue with react

--- lesson11_react_start/README.md
+++ lesson11_react_start/README.md
@@ -8,7 +8,12 @@ Open [http://localhost:3000](http://localhost:3000) to view it in your browser. 
 [https://react.dev/](https://react.dev/) - official react page with guide.
 [https://excalidraw.com/#json=LHqeOArwi5i9qPF21YWt4,a7194N7JF0OcoAYY2QmkJQ](https://excalidraw.com/#json=LHqeOArwi5i9qPF21YWt4,a7194N7JF0OcoAYY2QmkJQ) - excalidraw from lesson
 
-## Homework
+## Previous homework 
 - Start the app. (don't forget to install dependencies `npm install`)
 - Build the app.
 - Read a little bit about React from link above (as much as you want and can). After try to change something in the app that is created by create-react-app.
+
+## Homework
+- Using array of arrays called 'groups' (from App.jsx) where 1 array is 1 group of students. Create a list of all students (from all groups).
+- Using same array print only names of students with age > 12.
+- Using same array print only names of students who has at least 1 'A' grade.
---
+++ lesson11_react_start/first_app/src/App.jsx
@@ -0,0 +1,34 @@
+// import logo from './logo.svg';
+// const logo = reqiure('./logo.svg');
+import { Children } from 'react';
+import './App.css';
+
+function App() {
+  const fruits = ['apple', 'banana', 'orange'];
+  // .map is a method of array that returns a new array with the results of calling a provided function on every element in the calling array.
+  // const fruits2 = fruits.map( (fruit) => fruit[0] );
+  // const listItems = fruits.map( (fruit) => <li>{fruit}</li>)
+  // fruits -> listItems -> <ul>
+  // fruits -> <ul>
+  // console.log(fruits2);
+  const cars = [{ name: 'BMW', price: 13000 }, { name: 'Audi', price: 10000 }, { name: 'Mercedes', price: 15000 }];
+  const number = 10;
+  const groups = [[{name: 'Max', age: 12, grades:['A', 'A', 'A']}, {name: 'Joe', age: 13, grades:['B', 'C', 'A']}],
+   [{name: 'Jakson', age: 16, grades:['F', 'F', 'F']}, {name: 'Carl', age: 10, grades:['B', 'C', 'B']}],
+    [{name: 'Mike', age: 14, grades:['A', 'F', 'A']}, {name: 'Jane', age: 11, grades:['B', 'C', 'B']}]];
+  return (
+    <div>
+      <p>Hello world</p>
+      {/* condition          if true, do this         if false, do this*/}
+      {fruits.length > 0 ? "I have some fruits" : "I don't have any fruits "}
+      {number < 7 ? "small" : (number > 12 ? 'super big' : 'big')}
+      <ul>{fruits.map((fruit) => <li key = {fruit}>{fruit}</li>)}</ul>
+      {/* number<7 ->small, number > 7 -> big  */}
+      <p>Cars with price more then 12k:</p>
+      {/* {cars.price > 12000 ? (cars.name) : null} */}
+      <ul>{cars.map((car) => car.price > 12000 ? <li key={car.name}>{car.name}</li> : null)}</ul>
+    </div>
+  );
+}
+
+export { App };
