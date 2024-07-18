ac124e29134781156c47545cb1acba7b6d74183f
Lilly <Lillymarleen2010@gmail.com>
2024-07-18 20:12:26 +1000

    moving pieces to any place
    Co-authored-by: alukan <alukan@users.noreply.github.com>

--- chess-game/front-end/src/pages/game.jsx
+++ chess-game/front-end/src/pages/game.jsx
@@ -13,8 +13,18 @@ const initialBoard = [
 ];
 
 const pieceSymbols = {
-    'R': '♜', 'N': '♞', 'B': '♝', 'Q': '♛', 'K': '♚', 'P': '♟',
-    'r': '♖', 'n': '♘', 'b': '♗', 'q': '♕', 'k': '♔', 'p': '♙'
+  R: "♜",
+  N: "♞",
+  B: "♝",
+  Q: "♛",
+  K: "♚",
+  P: "♟",
+  r: "♖",
+  n: "♘",
+  b: "♗",
+  q: "♕",
+  k: "♔",
+  p: "♙",
 };
 
 // 1, 2, 3, 4
@@ -24,59 +34,102 @@ const pieceSymbols = {
 function Game() {
   const [board, setBoard] = useState(initialBoard);
   const [selectedPiece, setSelectedPiece] = useState(null);
-  const [currentTurn, setCurrentTurn] = useState('white');
+  const [currentTurn, setCurrentTurn] = useState("white");
 
   const handleCellClick = (row, cell) => {
     const piece = board[row][cell];
 
     // a -> A, A->A
     // A === A black piece, a !== A white piece
-    const pieceColor = piece && piece.toUpperCase() === piece ? 'black' : 'white'; 
+    const pieceColor =
+      (piece != null) ? (piece.toUpperCase() === piece ? "black" : "white") : null; // empty cell -> white
 
-    if(selectedPiece !== null){
-      if (pieceColor === currentTurn){
+    if (selectedPiece != null) {
+      if (pieceColor === currentTurn ) {
         setSelectedPiece([row, cell]);
+      } // move piece only if this block did not work
+      else {
+        movePiece(row, cell);
       }
+      //
     }
     // piece is not null (non empty), color of a piece is the same as current turn
-    else if(piece !== null && pieceColor === currentTurn) {
+    else if (piece != null && pieceColor === currentTurn) {
       // selectedPiece = [row, cell]
       setSelectedPiece([row, cell]);
+    }
+  };
+
+  const movePiece = (row, cell) => {
+    // [row, cell] - selected piece
+    //  0    1
+    // [3,   4]
+    const selectedRow = selectedPiece[0];
+    const selectedCell = selectedPiece[1];
+    const piece = board[selectedRow][selectedCell];// board[3][4]
+
+    if(piece.toUpperCase() == 'R'){
+      if(row != selectedRow && cell != selectedCell){
+        return;
+      }
+    }
       
-    } 
+    if (piece.toUpperCase() == 'B')
+      if(row + cell != selectedRow + selectedCell &&  row - cell != selectedRow - selectedCell)
+        return;
+    
+    
 
-
-  }
+    const copyBoard = [...board]; // ... - takes elements, [] - creates new array
+    
+    // step 1 - remove piece from old position, step 2 - put piece in new position
+    copyBoard[row][cell] = piece
+    copyBoard[selectedRow][selectedCell] = null;
+    
+    setBoard(copyBoard) // board = copyBoard
+    //delete a selection
+    setSelectedPiece(null);
+    if (currentTurn === 'white'){
+      setCurrentTurn('black');
+    }
+    else{
+      setCurrentTurn('white');
+    }
+    
+  };
 
   return (
     <div className="chessboard-container">
       {board.map((row, rowIndex) =>
-        row.map((cell, cellIndex) => 
+        row.map((cell, cellIndex) => (
           <div
-          //                                     setSelectedPiece([row, cell]);          [5, 6]
-            className={`cell-piece ${selectedPiece !== null && cellIndex == selectedPiece[1] && rowIndex == selectedPiece[0] ? 'selected' : '' }`}
+            //                                     setSelectedPiece([row, cell]);          [5, 6]
+            className={`cell-piece ${
+              selectedPiece !== null &&
+              cellIndex == selectedPiece[1] &&
+              rowIndex == selectedPiece[0]
+                ? "selected"
+                : ""
+            }`}
             key={`${rowIndex}-${cellIndex}`}
             style={{ gridRow: rowIndex + 1, gridColumn: cellIndex + 1 }}
             onClick={() => handleCellClick(rowIndex, cellIndex)}
-            >
-              
-            
-            {cell === null ? null : <span 
-              className="piece"  
-              style={{color: 'black'}}>
-              {pieceSymbols[cell]}
-              
-            </span>}
-    {/*expression ? works if true       :  works if false*/}
-            
+          >
+            {/* <div
+            className={`move-piece ${piece === selectedPiece && onClick={()=> handleCellClick(rowIndex, cellIndex)} ? movePiece : ''}`}
+            > */}
 
+            {cell === null ? null : (
+              <span className="piece" style={{ color: "black" }}>
+                {pieceSymbols[cell]}
+              </span>
+            )}
+            {/*expression ? works if true       :  works if false*/}
           </div>
-        )
-        )}
+        ))
+      )}
     </div>
   );
 }
-
-
 
 export default Game;
